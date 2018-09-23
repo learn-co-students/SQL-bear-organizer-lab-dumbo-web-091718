74159e0026ff65a6027802e18e787b5c67710541
Brian DiRito <bdirito@gmail.com>
2018-09-23 11:22:34 -0400

    Finished the lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("mr. Chocolate", 3, "male", "chocolate", "happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("rowdy", 4, "male", "black", "wild", "false");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("tabitha", 2, "female", "gray", "chill", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("sergeant brown", 7, "female", "brown", "feisty", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("melissa", 2, "female", "black", "hyper", "false");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("grinch", 9, "male", "black", "grouchy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("wendy", 3, "female", "chocolate", "chill", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 4, "male", "brown", "wild", "true");
