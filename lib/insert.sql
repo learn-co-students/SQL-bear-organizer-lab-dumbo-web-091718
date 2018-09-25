ce7e15e9ef770a69e4649745d1858cc06373197e
Carolyn Rosemary Winifred Cole <carolynrosemarywinifredcole@Carolyns-MacBook-Air.local>
2018-09-25 17:44:07 -0400

    I did it

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+  INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+  ("Sally", 12, "male", "red", "chill", "yes"),
+  ("Sam", 21, "female", "orange", "happy", "no"),
+  ("Mike", 44, "male", "blue", "dark", "yes"),
+  ("Winie", 50, "female", "green", "perky", "no"),
+  ("Sully", 12, "female", "indigo", "aggressive", "no"),
+  ("Rico", 27, "male", "black", "stoic", "yes"),
+  ("Phil", 24, "male", "white", "suspect", "no"),
+  (Null, 24, "female", "red", "energetic", "no");
